b20dc1dab25d01699353ade89e8fa7c4f23202d0
kartik <whonalekartik11@gmail.com>
2025-07-26 12:53:52 +0530

    edited mongondb file

--- config/db.js
+++ config/db.js
@@ -3,17 +3,24 @@ const { ServerApiVersion } = require('mongodb');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI , {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
         deprecationErrors: true,
-      }
+      },
+      retryWrites: true,
+      w: 'majority',
+      useNewUrlParser: true,
+      useUnifiedTopology: true
     });
 
+    // Send a ping to confirm a successful connection
+    await conn.connection.db.admin().command({ ping: 1 });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log('Pinged your deployment. You successfully connected to MongoDB!');
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(`MongoDB connection error: ${error.message}`);
     process.exit(1);
   }
 };
